9c8fba4642f91e7d3348800cd7ee65d9f6fbbb34
Brais Rodríguez González <brais.rodriguez.gonzalez@udc.es>
2024-01-31 01:08:01 +0100

    SQL para user y rol

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+-- Rol --
+INSERT INTO Rol (id, role)
+VALUES (1, 'ADMIN'),
+       (2, 'PRESIDENTE'),
+       (3, 'VICEPRESIDENTE'),
+       (4, 'TESORERO');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS UserRol;
+DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS Rol;
+
+CREATE TABLE Rol (
+    id   BIGINT      NOT NULL AUTO_INCREMENT,
+    role VARCHAR(20) NOT NULL,
+    CONSTRAINT RolPK PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+CREATE TABLE User (
+    id        BIGINT                         NOT NULL AUTO_INCREMENT,
+    userName  VARCHAR(60) COLLATE latin1_bin NOT NULL,
+    password  VARCHAR(60)                    NOT NULL,
+    firstName VARCHAR(60)                    NOT NULL,
+    lastName  VARCHAR(60)                    NOT NULL,
+    CONSTRAINT UserPK PRIMARY KEY (id),
+    CONSTRAINT UserNameUniqueKey UNIQUE (userName)
+) ENGINE = InnoDB;
+
+CREATE TABLE UserRol (
+    id     BIGINT NOT NULL AUTO_INCREMENT,
+    userId BIGINT NOT NULL,
+    rolId  BIGINT NOT NULL,
+    CONSTRAINT UserRolPK PRIMARY KEY (id),
+    CONSTRAINT FK_UserOfUserRol FOREIGN KEY (userId) REFERENCES User (id),
+    CONSTRAINT FK_RolOfUserRol FOREIGN KEY (rolId) REFERENCES Rol (id)
+) ENGINE = InnoDB;
