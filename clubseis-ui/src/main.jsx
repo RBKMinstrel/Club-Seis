a6e84a3317de15d415a6915505a97f9e2086b58b
Brais Rodríguez González <brais.rodriguez.gonzalez@udc.es>
2024-02-08 01:46:55 +0100

    Basico para desarrollo de la practica

--- clubseis-ui/src/main.jsx
+++ clubseis-ui/src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import {IntlProvider} from "react-intl";
+
+import store from './store';
+import app, {App} from './modules/app';
+import backend, {NetworkError} from './backend';
+import {initReactIntl} from './i18n';
+import './index.css';
+
+/* Configure backend proxy. */
+backend.init(() => store.dispatch(app.actions.error(new NetworkError())));
+
+/*Configure i18n.*/
+const {locale, messages} = initReactIntl();
+
+/*Render aplication*/
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App/>
+        <Provider store={store}>
+            <IntlProvider locale={locale} messages={messages}>
+                <BrowserRouter>
+                    <App/>
+                </BrowserRouter>
+            </IntlProvider>
+        </Provider>
     </React.StrictMode>,
 )
